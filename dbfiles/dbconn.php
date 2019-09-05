c579d6301c914327a9df1f94265f9667f704fdd1
Rahul Pandit More <more03625@gmail.com>
2019-09-05 16:13:02 +0530

    Update and rename test to dbconn.php

---
+++ dbfiles/dbconn.php
@@ -0,0 +1,8 @@
+<?php
+$host       = "localhost";
+$dbusername = "rahul";
+$dbpass     = "Rahul@123";
+$dbname     = "TheTrains";
+$conn       = mysqli_connect($host, $dbusername, $dbpass, $dbname);
+
+?>
