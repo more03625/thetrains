1e08092c67f48042bad3358c70caf067900dcd24
Rahul Pandit More <more03625@gmail.com>
2019-09-28 20:35:53 +0530

    arriving time added
    arriving time added for trains. so now we can directly get a current time then we will search for an available train, we already have an all the station names in the array.  it does not matter now from where the user is searching for a train cause we have added logic now "interval time" column

--- dbfiles/Hoarbour_Station_Names.sql
+++ dbfiles/Hoarbour_Station_Names.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:3306
--- Generation Time: Sep 18, 2019 at 02:26 PM
+-- Generation Time: Sep 28, 2019 at 08:30 PM
 -- Server version: 5.7.27-0ubuntu0.18.04.1
 -- PHP Version: 7.2.19-0ubuntu0.18.04.2
 
@@ -33,49 +33,40 @@ CREATE TABLE `stations` (
   `c` tinyint(1) NOT NULL DEFAULT '0',
   `w` tinyint(1) NOT NULL DEFAULT '0',
   `th` tinyint(1) NOT NULL DEFAULT '0',
-  `rank` text NOT NULL
+  `rank` text NOT NULL,
+  `intervaltime` time(6) DEFAULT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `stations`
 --
 
-INSERT INTO `stations` (`id`, `station_name`, `h`, `c`, `w`, `th`, `rank`) VALUES
-(1, '(CSMT) Chhatrapati Shivaji Maharaj Terminus', 1, 0, 0, 0, '1'),
-(2, 'Masjid Bandar', 1, 0, 0, 0, '2'),
-(3, 'Sandhurst Road', 1, 0, 0, 0, '3'),
-(4, 'Dockyard Road', 1, 0, 0, 0, '4'),
-(5, 'Reay Road', 1, 0, 0, 0, '5'),
-(6, 'Cotteon Green', 1, 0, 0, 0, '6'),
-(7, 'Sewri', 1, 0, 0, 0, '7'),
-(8, 'Wadala Road', 1, 0, 0, 0, '8'),
-(9, '(GTB) Guru Tegh Bahadur Nagar', 1, 0, 0, 0, '9'),
-(10, 'Chunabhatti', 1, 0, 0, 0, '10'),
-(11, 'Kurla', 1, 0, 0, 0, '11'),
-(12, 'Tilak Nagar', 1, 0, 0, 0, '12'),
-(13, 'Chembur', 1, 0, 0, 0, '13'),
-(14, 'Govandi', 1, 0, 0, 0, '14'),
-(15, 'Mankhurd', 1, 0, 0, 0, '15'),
-(16, 'Vashi', 1, 0, 0, 0, '16'),
-(17, 'Sanpada', 1, 0, 0, 0, '17'),
-(18, 'Juinagar', 1, 0, 0, 0, '18'),
-(19, 'Nerul', 1, 0, 0, 0, '19'),
-(20, 'Seawoods Darave', 1, 0, 0, 0, '20'),
-(21, 'CBD Belapur', 1, 0, 0, 0, '21'),
-(22, 'Kharghar', 1, 0, 0, 0, '22'),
-(23, 'Mansarovar', 1, 0, 0, 0, '23'),
-(24, 'Khandeshwar', 1, 0, 0, 0, '24'),
-(25, 'Panvel', 1, 0, 0, 0, '25'),
-(26, 'Kings Circle', 1, 0, 0, 0, '80'),
-(27, 'Mahim', 1, 0, 0, 0, '81'),
-(28, 'Bandra', 1, 0, 0, 0, '82'),
-(29, 'Khar Road', 1, 0, 0, 0, '83'),
-(30, 'Santacruz', 1, 0, 0, 0, '84'),
-(31, 'Ville Parle', 1, 0, 0, 0, '85'),
-(32, 'Andheri', 1, 0, 0, 0, '86'),
-(33, 'Jogeshwari', 1, 0, 0, 0, '87'),
-(34, 'Ram Mandir', 1, 0, 0, 0, '88'),
-(35, 'Goregaon', 1, 0, 0, 0, '89');
+INSERT INTO `stations` (`id`, `station_name`, `h`, `c`, `w`, `th`, `rank`, `intervaltime`) VALUES
+(1, 'CSMT', 1, 0, 0, 0, '1', '00:00:00.000000'),
+(2, 'Masjid Bandar', 1, 0, 0, 0, '2', '00:03:00.000000'),
+(3, 'Sandhurst Road', 1, 0, 0, 0, '3', '00:06:00.000000'),
+(4, 'Dockyard Road', 1, 0, 0, 0, '4', '00:08:00.000000'),
+(5, 'Reay Road', 1, 0, 0, 0, '5', '00:10:00.000000'),
+(6, 'Cotteon Green', 1, 0, 0, 0, '6', '00:12:00.000000'),
+(7, 'Sewri', 1, 0, 0, 0, '7', '00:15:00.000000'),
+(8, 'Wadala Road', 1, 0, 0, 0, '8', '00:18:00.000000'),
+(9, '(GTB) Guru Tegh Bahadur Nagar', 1, 0, 0, 0, '9', '00:23:00.000000'),
+(10, 'Chunabhatti', 1, 0, 0, 0, '10', '00:26:00.000000'),
+(11, 'Kurla', 1, 0, 0, 0, '11', '00:29:00.000000'),
+(12, 'Tilak Nagar', 1, 0, 0, 0, '12', '00:32:00.000000'),
+(13, 'Chembur', 1, 0, 0, 0, '13', '00:34:00.000000'),
+(14, 'Govandi', 1, 0, 0, 0, '14', '00:37:00.000000'),
+(15, 'Mankhurd', 1, 0, 0, 0, '15', '00:40:00.000000'),
+(16, 'Vashi', 1, 0, 0, 0, '16', '00:49:00.000000'),
+(17, 'Sanpada', 1, 0, 0, 0, '17', '00:51:00.000000'),
+(18, 'Juinagar', 1, 0, 0, 0, '18', '00:54:00.000000'),
+(19, 'Nerul', 1, 0, 0, 0, '19', '00:58:00.000000'),
+(20, 'Seawoods Darave', 1, 0, 0, 0, '20', '01:01:00.000000'),
+(21, 'CBD Belapur', 1, 0, 0, 0, '21', '01:05:00.000000'),
+(22, 'Kharghar', 1, 0, 0, 0, '22', '01:09:00.000000'),
+(23, 'Mansarovar', 1, 0, 0, 0, '23', '01:12:00.000000'),
+(24, 'Khandeshwar', 1, 0, 0, 0, '24', '01:15:00.000000'),
+(25, 'Panvel', 1, 0, 0, 0, '25', '01:20:00.000000');
 
 --
 -- Indexes for dumped tables
